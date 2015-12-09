af34f86a4c8563ac1be74b307d2a68274dab234d
Bruce Becker <brucellino@gmail.com>
2015-12-09 10:40:50 +0100

    Wrong name of the source file

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ mkdir -p ${SOFT_DIR}
 if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
   echo "seems like this is the first build - let's get the source"
-  wget http://www.fftw.org/fftw-${VERSION} -O ${SRC_DIR}/${SOURCE_FILE}
+  wget http://www.fftw.org/fftw-${VERSION}.tar.gz -O ${SRC_DIR}/${SOURCE_FILE}
   echo "releasing lock"
   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
 elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
